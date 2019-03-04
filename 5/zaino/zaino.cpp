5de7457648d6376443af732e14505b21654f42be
Francesco Tomaselli <tomasellifrancesco1298@gmail.com>
2019-03-04 13:02:15 +0100

    test

---
+++ 5/zaino/zaino.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+  ifstream in("input.txt");
+  ofstream out("output.txt");
+
+  int n,c;
+  in >> n >> c;
+
+  vector<vector<int>> dp(n);
+  for (int i = 0; i < n; i++) {
+    dp[i]
+  }
+
+  return 0;
+}
