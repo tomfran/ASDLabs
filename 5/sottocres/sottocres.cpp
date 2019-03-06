e867a6ca401f7732e00db9feef5d6ba53b09c9ff
Francesco Tomaselli <tomasellifrancesco1298@gmail.com>
2019-03-06 08:23:01 +0100

    sottocres 60%

--- 5/sottocres/sottocres.cpp
+++ 5/sottocres/sottocres.cpp
@@ -1,14 +1,37 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-int sottocres();
+int sottocres(vector<int> v, int lim, int i, int n, map<pair<int,int>,int>& DP);
 
 int main(){
   ifstream in("input.txt");
   ofstream out("output.txt");
 
-  out << sottocres();
+  int n;
+  in >> n;
+
+  vector<int> v(n);
+  for (size_t i = 0; i < n; i++)
+    in >> v[i];
+
+  map<pair<int,int>,int> DP;
+  out << sottocres(v,0,0,n,DP);
   return 0;
+}
+
+int sottocres(vector<int> v, int lim, int i, int n, map<pair<int,int>,int>& DP){
+  if(i>=n)
+    return 0;
+  else if (v[i]<lim)
+    return sottocres(v,lim,i+1,n,DP);
+  else if(DP.find({i,lim})==DP.end()){
+    int taken = v[i] + sottocres(v,v[i],i+1,n,DP);
+    int nottaken = sottocres(v,lim,i+1,n,DP);
+    DP[{i,lim}] = max(taken,nottaken);
+  }
+  return DP[{i,lim}];
+}
