d66db647d5157a09c1a43ba04da9fc57c2470719
Francesco <tomasellifrancesco1298@gmail.com>
2019-04-02 10:00:31 +0200

    Sherlock 80%

--- 6/sherlock/sherlock2.cpp
+++ 6/sherlock/sherlock2.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 vector<int> parser(string a);
-int sherlock(vector<vector<int>>& DP, vector<vector<int>>& DPAUX, vector<vector<int>>& nights, int k, int i);
-int scherlockAux(vector<int>& s, int k, int i,vector<vector<int>>& DPAUX);
+int scherlockAux(vector<int>& s, int k);
+int sherlock(vector<vector<int>>& DP, vector<vector<int>>& nights, int k, int i);
 
 int main(){
   ifstream in("input.txt");
@@ -20,12 +20,9 @@ int main(){
     nights[i] = parser(tmp);
   }
   vector<vector<int>> DP(t+1,vector<int> (n,-1));
-  vector<vector<int>> DPAUX(t+1,vector<int> (n,-1));
 
-  for (int i = 0; i < DP[0].size(); i++) {
-    DP[0][i] = DPAUX[0][i] = 0;
-  }
-  out << sherlock(DP,DPAUX,nights,t,0);
+  out << sherlock(DP,nights,t,0);
+
 
   return 0;
 }
@@ -47,42 +44,40 @@ vector<int> parser(string a){
   return v;
 }
 
-int sherlock(vector<vector<int>>& DP, vector<vector<int>>& DPAUX, vector<vector<int>>& nights, int k, int i){
+int sherlock(vector<vector<int>>& DP, vector<vector<int>>& nights, int k, int i){
   if(i>=nights.size())
     return 0;
   if(DP[k][i]==-1){
     int s = 0;
     for (int r = 0; r <= k; r++)
-      s = max(s,scherlockAux(nights[i],k-r,i,DPAUX)+sherlock(DP,DPAUX,nights,r,i+1));
+      s = max(s,scherlockAux(nights[i],k-r)+sherlock(DP,nights,r,i+1));
     DP[k][i] = s;
   }
   return DP[k][i];
 }
 
-int scherlockAux(vector<int>& s, int k, int i,vector<vector<int>>& DPAUX){
-  if (DPAUX[k][i]==-1){
+int scherlockAux(vector<int>& s, int k){
+  if (k == 0)
+  return 0;
+  else{
     //s[0] perchè il primo può solo scegliere se stesso
-    vector<vector<int>> DP(2,vector<int> (s.size(),s[0]));
+    vector<vector<int>> DP(k,vector<int> (s.size(),s[0]));
 
     //k = 0 somma alternata
     for (int i = 0; i < 2 && i<DP[0].size(); i++)
-      DP[0][i] = s[i];
+    DP[0][i] = s[i];
     for (int i = 2; i < DP[0].size(); i++)
-      DP[0][i] = s[i] + DP[0][i-2];
+    DP[0][i] = s[i] + DP[0][i-2];
 
     //la condizione max sul secondo restituirà sempre quel caso,
     //per gli altri calcolo quello che mi conviene
-    for (int t = 1; t < k; t++) {
-      DP[1][1] = DP[0][0]+s[1];
+    for (int k = 1; k < DP.size(); k++) {
+      DP[k][1] = DP[k-1][0]+s[1];
       for (int i = 2; i < DP[0].size(); i++)
-        DP[1][i] = max(DP[0][i-1]+s[i],max(DP[1][i-1],DP[1][i-2]+s[i]));
-      DP[0] = DP[1];
+        DP[k][i] = max(DP[k-1][i-1]+s[i],max(DP[k][i-1],DP[k][i-2]+s[i]));
     }
+
     // max si trova nell'ultima posizione
-    if(k>1)
-      DPAUX[k][i] = DP[1][s.size()-1];
-    else
-      DPAUX[k][i] = max(DP[0][s.size()-1],DP[0][s.size()-2]);
+    return DP[k-1][s.size()-1];
   }
-  return DPAUX[k][i];
 }
